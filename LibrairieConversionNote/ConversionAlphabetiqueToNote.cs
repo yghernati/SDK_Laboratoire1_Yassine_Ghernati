ba239059fc838d739314023360a063356ca8d0a8
yassine ghernati <twos1001@gmail.com>
2023-06-16 19:59:18 -0400

    Creation de la librairie de conversion et validation des inputs

---
+++ LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace LibrairieConversionNote
+{
+    public static class ConversionAlphabetiqueToNote
+    {
+        public static string Transform(string noteAlphabetique)
+        {
+            string note = "";
+
+            switch (noteAlphabetique.ToUpper())
+            {
+                case "A":
+                    note = "la syllabe correspondante a votre choix est : LA";
+                    break;
+                case "B":
+                    note = "la syllabe correspondante a votre choix est : SI";
+                    break;
+                case "C":
+                    note = "la syllabe correspondante a votre choix est : DO";
+                    break;
+                case "D":
+                    note = "la syllabe correspondante a votre choix est : RÉ";
+                    break;
+                case "E":
+                    note = "la syllabe correspondante a votre choix est : MI";
+                    break;
+                case "F":
+                    note = "la syllabe correspondante a votre choix est : FA";
+                    break;
+                case "G":
+                    note = "la syllabe correspondante a votre choix est : SOL";
+                    break;
+                default:
+                    note = "aucune syllabe ne correspond a votre choix !!! reessayer !!!";
+                    break;
+            }
+
+            return note;
+        }
+    }
+}
--- SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibrairieConversionNote;
+
+while (true)
+{
+    Console.WriteLine("Veuillez entrer une note alphabétique (ou appuyez sur CTRL + C pour quitter) :");
+    string input = Console.ReadLine();
+
+    string note = ConversionAlphabetiqueToNote.Transform(input);
+
+    Console.WriteLine(note);
+}
